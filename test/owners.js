32a19734a329b70af51481af1e7d816716c8c2a8
montag_v <valentin.montagne@epitech.eu>
2018-07-02 16:36:15 +0200

    Adding options to get Owners

--- test/owners.js
+++ test/owners.js
@@ -6,8 +6,14 @@ const hubspot = new Hubspot({ apiKey: 'demo' })
 
 describe('Owners', function () {
   describe('get', function () {
-    it('Should return all owners', function () {
+    it('Should return all owners', () => {
       return hubspot.owners.get().then(data => {
+        expect(data).to.be.a('array')
+      })
+    })
+
+    it('Should return all owners and inactives', () => {
+      return hubspot.owners.get({ inactives: true }).then(data => {
         expect(data).to.be.a('array')
       })
     })
